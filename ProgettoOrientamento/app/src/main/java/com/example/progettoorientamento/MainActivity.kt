b5f323644fc750e54d4da4db5ff435a3c1c3acf3
ZhengWenJun01 <126774168+ZhengWenJun01@users.noreply.github.com>
2023-10-30 12:14:52 +0100

    Commit 2
    Aggiornamento

--- ProgettoOrientamento/app/src/main/java/com/example/progettoorientamento/MainActivity.kt
+++ ProgettoOrientamento/app/src/main/java/com/example/progettoorientamento/MainActivity.kt
@@ -5,6 +5,10 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.snackbar.Snackbar
+
+
+
 
 
 class MainActivity : AppCompatActivity() {
@@ -17,10 +21,27 @@ class MainActivity : AppCompatActivity() {
         var loginButton = findViewById<Button>(R.id.loginButton)
 
         loginButton.setOnClickListener {
-            if(passwordField.text.toString() == "12345678"){
-                val intent = Intent(this, HomeActivity::class.java)
-                startActivity(intent)
+            if(emailField.text.toString() == "italia"){
+                if(passwordField.text.toString() == "12345678"){
+                    val intent = Intent(this, HomeActivity::class.java)
+                    startActivity(intent)
+                } else {
+                    val snackbar = Snackbar.make(
+                        findViewById(android.R.id.content),
+                        "password invalido",
+                        Snackbar.LENGTH_LONG
+                    )
+                    snackbar.show()
+                }
+            }else{
+                val snackbar = Snackbar.make(
+                    findViewById(android.R.id.content),
+                    "email invalido",
+                    Snackbar.LENGTH_LONG
+                )
+                snackbar.show()
             }
+
         }
 
     }
